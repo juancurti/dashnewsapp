77513966471e7ff7bcbeaf95827be2b33194d157
Juan <juancurti.it@gmail.com>
2021-05-04 01:35:29 +0200

    Fix on websites

--- lib/views/WebViewScreen.dart
+++ lib/views/WebViewScreen.dart
@@ -1,6 +1,12 @@
+import 'dart:io';
+
+import 'package:dashnews/data/ThemeHandler.dart';
 import 'package:dashnews/data/controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+import 'HomeScreen.dart';
 
 class WebViewScreen extends StatefulWidget {
   WebViewScreen({Key key, this.item}) : super(key: key);
@@ -12,6 +18,37 @@ class WebViewScreen extends StatefulWidget {
 
 class _WebViewScreenState extends State<WebViewScreen> {
   final ControllerSession appController = Get.find();
+  String _loadUrl;
+
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+    this.loadData();
+  }
+
+  void loadData() {
+    if (widget.item['url'] != null) {
+      setState(() {
+        _loadUrl =
+            widget.item['url'].toString().replaceAll('old.reddit', 'm.reddit');
+      });
+    } else if (widget.item['url_overridden_by_dest'] != null) {
+      setState(() {
+        _loadUrl = widget.item['url_overridden_by_dest']
+            .toString()
+            .replaceAll('old.reddit', 'm.reddit');
+      });
+    } else {
+      Get.back();
+    }
+
+    print(_loadUrl);
+  }
+
+  void addBookmark() {}
+
+  void share() {}
 
   @override
   Widget build(BuildContext context) {
@@ -19,20 +56,140 @@ class _WebViewScreenState extends State<WebViewScreen> {
         backgroundColor: Color.fromRGBO(19, 20, 21, 1),
         body: SafeArea(
           child: Container(
-            padding: EdgeInsets.only(top: 40),
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Text(
-                  'WebView',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 22,
-                  ),
-                ),
+                Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 80,
+                    decoration: BoxDecoration(
+                      color: ThemeHandler.getTopBarColor(
+                          dark: appController.darkMode.value),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withAlpha(100),
+                          blurRadius: 20.0,
+                          offset: new Offset(0.0, 5.0),
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      children: [
+                        SizedBox(width: 20),
+                        Container(
+                            width: MediaQuery.of(context).size.width - 40 - 50,
+                            child: Row(
+                              children: [
+                                InkWell(
+                                    onTap: () {
+                                      Get.back();
+                                    },
+                                    child: Container(
+                                      height: 60,
+                                      width: 50,
+                                      child: Center(
+                                          child: Icon(
+                                        Icons.arrow_back,
+                                        size: 32,
+                                        color: ThemeHandler.getBackgroundColor(
+                                            dark: appController.darkMode.value),
+                                      )),
+                                    ))
+                              ],
+                            )),
+                        Container(
+                          width: 50,
+                          height: 60,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              PopupMenuButton<String>(
+                                  child: Container(
+                                    height: 25,
+                                    width: 25,
+                                    decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            image: AssetImage(
+                                                'assets/options-icon.png'))),
+                                  ),
+                                  color: ThemeHandler.getDropdownColor(
+                                      dark: appController.darkMode.value),
+                                  onSelected: (val) {
+                                    if (val == 'share') {
+                                      this.share();
+                                    } else if (val == 'addbookmark') {
+                                      this.addBookmark();
+                                    }
+                                  },
+                                  itemBuilder: (BuildContext context) {
+                                    return [
+                                      PopupMenuItem(
+                                        child: Row(
+                                          children: [
+                                            Container(
+                                              width: 20,
+                                              height: 20,
+                                              decoration: BoxDecoration(
+                                                  image: DecorationImage(
+                                                      image: AssetImage(
+                                                          'assets/bookmarks-icon.png'))),
+                                            ),
+                                            SizedBox(width: 10),
+                                            Text(
+                                              'Add Bookmark',
+                                              style: TextStyle(
+                                                  color: ThemeHandler
+                                                      .getDropdownTextColor(
+                                                          dark: appController
+                                                              .darkMode.value)),
+                                            )
+                                          ],
+                                        ),
+                                        value: 'addbookmark',
+                                      ),
+                                      PopupMenuItem(
+                                        child: Row(
+                                          children: [
+                                            Container(
+                                              width: 20,
+                                              height: 20,
+                                              decoration: BoxDecoration(
+                                                  image: DecorationImage(
+                                                      image: AssetImage(
+                                                          'assets/new-share-icon.png'))),
+                                            ),
+                                            SizedBox(width: 10),
+                                            Text(
+                                              'Share',
+                                              style: TextStyle(
+                                                  color: ThemeHandler
+                                                      .getDropdownTextColor(
+                                                          dark: appController
+                                                              .darkMode.value)),
+                                            )
+                                          ],
+                                        ),
+                                        value: 'share',
+                                      )
+                                    ];
+                                  })
+                            ],
+                          ),
+                        )
+                      ],
+                    )),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height - 80,
+                  child: _loadUrl == null
+                      ? SizedBox()
+                      : WebView(
+                          initialUrl: _loadUrl,
+                        ),
+                )
               ],
             ),
           ),

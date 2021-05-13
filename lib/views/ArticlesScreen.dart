b9da91151e5b97040463a3f37fa3d1af3293a8cd
Juan <juancurti.it@gmail.com>
2021-05-13 18:52:36 +0200

    Learning view

--- lib/views/ArticlesScreen.dart
+++ lib/views/ArticlesScreen.dart
@@ -1,4 +1,8 @@
+import 'package:dashnews/data/RequestHandler.dart';
+import 'package:dashnews/data/ThemeHandler.dart';
 import 'package:dashnews/data/controller.dart';
+import 'package:dashnews/views/HomeScreen.dart';
+import 'package:dashnews/views/WebViewScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -6,34 +10,395 @@ class ArticlesScreen extends StatefulWidget {
   ArticlesScreen({Key key}) : super(key: key);
 
   @override
-  _ArticlesScreenState createState() => _ArticlesScreenState();
+  _MainScreenState createState() => _MainScreenState();
 }
 
-class _ArticlesScreenState extends State<ArticlesScreen> {
+class _MainScreenState extends State<ArticlesScreen> {
   final ControllerSession appController = Get.find();
+  TextEditingController searchController = TextEditingController();
+  List<dynamic> _originalList = [
+    {
+      'title': 'Masternodes',
+      'asset': 'assets/masternodes.png',
+      'last': '13th May 2020',
+      'articles': [
+        {
+          'title': 'Dash.org: Masternodes',
+          'url': 'https://www.dash.org/masternodes/'
+        },
+        {
+          'title': 'Understanding Masternodes',
+          'url':
+              'https://docs.dash.org/en/stable/masternodes/understanding.html'
+        }
+      ]
+    },
+    {
+      'title': 'Governance',
+      'asset': 'assets/governance.png',
+      'last': '13th May 2020',
+      'articles': [
+        {
+          'title': 'Understanding Governance',
+          'url': 'https://docs.dash.org/en/stable/governance/understanding.html'
+        },
+        {
+          'title': 'Using Dash Governance',
+          'url':
+              'https://aywa-platform-docs.readthedocs.io/en/latest/governance/using.html'
+        }
+      ]
+    },
+    {
+      'title': 'Dash Mining',
+      'asset': 'assets/dashmining.png',
+      'last': '13th May 2020',
+      'articles': []
+    },
+    {
+      'title': 'Decentralized Apps',
+      'asset': 'assets/dapps.png',
+      'last': '13th May 2020',
+      'articles': []
+    },
+    {
+      'title': 'Dash Platform',
+      'asset': 'assets/dashplat.png',
+      'last': '13th May 2020',
+      'articles': []
+    },
+    {
+      'title': 'Dash Incubator',
+      'asset': 'assets/dashincub.png',
+      'last': '13th May 2020',
+      'articles': []
+    },
+    {
+      'title': 'Trading Dash',
+      'asset': 'assets/traddash.png',
+      'last': '13th May 2020',
+      'articles': []
+    },
+    {
+      'title': 'Dash Features',
+      'asset': 'assets/masternodes.png',
+      'last': '13th May 2020',
+      'articles': []
+    },
+    {
+      'title': 'Wallet Options',
+      'asset': 'assets/masternodes.png',
+      'last': '13th May 2020',
+      'articles': []
+    },
+    {
+      'title': 'Safety',
+      'asset': 'assets/masternodes.png',
+      'last': '13th May 2020',
+      'articles': []
+    },
+  ];
+  List<dynamic> _filteredList = [];
+  bool showSearch = false;
+  int indexSelected;
+
+  @override
+  void initState() {
+    super.initState();
+    this.load();
+  }
+
+  void load() {
+    this.setState(() {
+      _filteredList = _originalList;
+    });
+  }
+
+  void doSearch() {
+    List<dynamic> _newItems = _originalList
+        .where((element) => element['title']
+            .toString()
+            .toUpperCase()
+            .contains(searchController.text.toUpperCase()))
+        .toList();
+    this.setState(() {
+      _filteredList = _newItems;
+    });
+  }
+
+  Widget getCategoryItem({Map<String, dynamic> item}) {
+    List<dynamic> articles = item['articles'];
+    return Column(
+      children: [
+        InkWell(
+            onTap: () {
+              if (indexSelected == this._filteredList.indexOf(item)) {
+                this.setState(() {
+                  indexSelected = null;
+                });
+              } else {
+                this.setState(() {
+                  indexSelected = this._filteredList.indexOf(item);
+                });
+              }
+            },
+            child: Container(
+              width: MediaQuery.of(context).size.width - 40,
+              height: 80,
+              margin: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                      height: 80,
+                      width: (MediaQuery.of(context).size.width - 40) * 0.3,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage(item['asset'])),
+                      )),
+                  Container(
+                    height: 80,
+                    width: ((MediaQuery.of(context).size.width - 40) * 0.7) - 2,
+                    color: ThemeHandler.getBottomBarColor(
+                        dark: appController.darkMode.value),
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          top: 10,
+                          left: 10,
+                          child: Container(
+                            width: (((MediaQuery.of(context).size.width - 40) *
+                                    0.7) -
+                                62),
+                            child: Text(
+                              item['title'],
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                                color: ThemeHandler.getTextColor(
+                                    dark: appController.darkMode.value),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 6,
+                          left: 10,
+                          child: Container(
+                            width: (((MediaQuery.of(context).size.width - 40) *
+                                    0.7) -
+                                30),
+                            child: Text(
+                              '${item['articles'].length} Articles | Last updated: ${item['last']}',
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w400,
+                                color: ThemeHandler.getTextColor(
+                                        dark: appController.darkMode.value)
+                                    .withAlpha(100),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )),
+        Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: articles
+                .map((e) => getArticleButton(item: item, thisItem: e))
+                .toList())
+      ],
+    );
+  }
+
+  Widget getArticleButton(
+      {Map<String, dynamic> item, Map<String, dynamic> thisItem}) {
+    return AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeOut,
+        height: this.indexSelected == this._filteredList.indexOf(item) ? 30 : 0,
+        margin: EdgeInsets.symmetric(
+            vertical: this.indexSelected == this._filteredList.indexOf(item)
+                ? 10
+                : 0),
+        width: MediaQuery.of(context).size.width * 0.6,
+        child: Center(
+          child: InkWell(
+            child: Text(thisItem['title']),
+            onTap: () {
+              Get.to(WebViewScreen(
+                item: thisItem,
+              ));
+            },
+          ),
+        ));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromRGBO(19, 20, 21, 1),
-      body: Container(
-        padding: EdgeInsets.only(top: 40),
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'Articles',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 22,
-              ),
+    return Obx(() => Scaffold(
+          backgroundColor: ThemeHandler.getBackgroundColor(
+              dark: appController.darkMode.value),
+          body: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 80,
+                    decoration: BoxDecoration(
+                      color: ThemeHandler.getTopBarColor(
+                          dark: appController.darkMode.value),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withAlpha(100),
+                          blurRadius: 20.0,
+                          offset: new Offset(0.0, 5.0),
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      children: [
+                        SizedBox(width: 20),
+                        Container(
+                          width: MediaQuery.of(context).size.width - 40 - 50,
+                          child: Text(
+                            'Learning',
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              color: Colors.white,
+                              fontSize: 22,
+                            ),
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            this.setState(() {
+                              showSearch = !showSearch;
+                            });
+                          },
+                          child: Container(
+                            height: 25,
+                            width: 25,
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image:
+                                        AssetImage('assets/search-icon.png'))),
+                          ),
+                        ),
+                      ],
+                    )),
+                Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height - 160,
+                    child: Stack(
+                      children: [
+                        ListView(
+                          children: [
+                            AnimatedContainer(
+                              duration: Duration(milliseconds: 500),
+                              curve: Curves.easeOut,
+                              width: 2,
+                              height: showSearch ? 60 : 1,
+                            ),
+                            Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: this._filteredList.length == 0
+                                    ? [SizedBox()]
+                                    : this
+                                        ._filteredList
+                                        .map((e) =>
+                                            this.getCategoryItem(item: e))
+                                        .toList())
+                          ],
+                        ),
+                        showSearch
+                            ? Positioned(
+                                top: 0,
+                                left: 0,
+                                child: Container(
+                                  width: MediaQuery.of(context).size.width,
+                                  height: 50,
+                                  color: ThemeHandler.getDropdownColor(
+                                      dark: appController.darkMode.value),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      Container(
+                                        width:
+                                            (MediaQuery.of(context).size.width *
+                                                    0.8) -
+                                                60,
+                                        color: ThemeHandler.getDropdownColor(
+                                            dark: appController.darkMode.value),
+                                        child: TextField(
+                                          controller: searchController,
+                                          onSubmitted: (str) {
+                                            this.doSearch();
+                                          },
+                                          style: TextStyle(
+                                              color: ThemeHandler
+                                                  .getDropdownTextColor(
+                                                      dark: appController
+                                                          .darkMode.value)),
+                                          decoration: InputDecoration.collapsed(
+                                              hintText: 'e.g: Venezuela',
+                                              hintStyle: TextStyle(
+                                                  color: ThemeHandler
+                                                      .getDropdownTextColor(
+                                                          dark: appController
+                                                              .darkMode
+                                                              .value))),
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        width: 10,
+                                      ),
+                                      InkWell(
+                                          onTap: () {
+                                            this.doSearch();
+                                          },
+                                          child: Container(
+                                              height: 30,
+                                              width: 30,
+                                              decoration: BoxDecoration(
+                                                  color: Color.fromRGBO(
+                                                      0, 116, 187, 1),
+                                                  borderRadius:
+                                                      BorderRadius.circular(6)),
+                                              child: Center(
+                                                child: Container(
+                                                    width: 20,
+                                                    height: 20,
+                                                    decoration: BoxDecoration(
+                                                        image: DecorationImage(
+                                                            image: AssetImage(
+                                                                'assets/search-icon.png')))),
+                                              )))
+                                    ],
+                                  ),
+                                ),
+                              )
+                            : SizedBox()
+                      ],
+                    ))
+              ],
             ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
